5f85ea736958a997b813b71f15ebaeaa8d77cacb
Mai Tuyen <maituyen4496@gmail.com>
2025-10-07 23:07:38 +0700

    custom header logo

--- src/app/Layout/Header.tsx
+++ src/app/Layout/Header.tsx
@@ -8,11 +8,13 @@ import Image from 'next/image'
 import { storage } from '@/utils/supabase/storage'
 import { LoginModal } from '@/app/auth/login/ModalLogin'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
+import { cn } from '@/lib/utils'
 export default function Header() {
   const [user, setUser] = useState<User | null>(null)
   const [loading, setLoading] = useState(true)
   const supabase = createClient()
   const [open, setOpen] = useState(false)
+  const [isAtTop, setIsAtTop] = useState(true)
   useEffect(() => {
     // Get initial session
     const getInitialSession = async () => {
@@ -37,6 +39,15 @@ export default function Header() {
     return () => subscription.unsubscribe()
   }, [supabase])
 
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsAtTop(window.scrollY <= 0)
+    }
+    handleScroll()
+    window.addEventListener('scroll', handleScroll, { passive: true })
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
   const handleSignOut = async () => {
     await supabase.auth.signOut()
   }
@@ -47,19 +58,36 @@ export default function Header() {
 
   return (
     <header className='sticky top-0 z-10 border-b border-gray-200 bg-white shadow-2xl backdrop-blur-sm dark:border-gray-700 dark:bg-gray-900/80'>
-      <div className='mx-auto max-w-7xl px-4 py-4 sm:px-6 lg:px-8'>
-        <div className='flex items-center justify-between'>
-          <Link href='/' className='flex items-center space-x-2'>
-            <Image src='/images/logo.png' alt='Logo' width={30} height={30} />
-            <span className='text-primary text-xl font-bold dark:text-white'>ZoloQuiz</span>
-          </Link>
-          <nav className='hidden items-center space-x-6 md:flex'>
+      <div className='mx-auto max-w-7xl px-4 py-2 sm:px-6 lg:px-8'>
+        <div className='relative flex items-center justify-between'>
+          <div className='relative w-full'>
+            <Link
+              href='/'
+              className={cn(
+                'flex items-center space-x-2',
+                isAtTop
+                  ? 'absolute bottom-[-80px] w-[260px] rounded-full bg-white/95 p-2 backdrop-blur-lg duration-300 hover:scale-105'
+                  : ''
+              )}
+            >
+              {isAtTop ? (
+                <Image src='/images/logo.png' alt='Logo' width={100} height={100} />
+              ) : (
+                <Image src='/images/logo.png' alt='Logo' width={30} height={30} />
+              )}
+              <div className='flex flex-col'>
+                <span className='text-primary text-xl font-bold dark:text-white'>ZoloQuiz</span>
+                {isAtTop && <span className='text-primary text-xs font-bold dark:text-white'>Know more daily</span>}
+              </div>
+            </Link>
+          </div>
+          <nav className='hidden shrink-0 items-center space-x-6 md:flex'>
             {loading ? (
               <div className='h-6 w-6 animate-spin rounded-full border-2 border-gray-300 border-t-blue-600'></div>
             ) : user ? (
               <DropdownMenu>
                 <DropdownMenuTrigger className='cursor-pointer'>
-                  <div className='flex items-center space-x-2'>
+                  <div className='flex items-center space-x-2 rounded-full border border-gray-200 p-1 px-2'>
                     <Image
                       src={user.user_metadata?.avatar_url}
                       alt='User Avatar'
@@ -67,7 +95,7 @@ export default function Header() {
                       height={32}
                       className='border-gradient-to-r rounded-full border-2 from-blue-600 to-purple-600'
                     />
-                    <span className='text-primary text-sm dark:text-gray-300'>
+                    <span className='shrink-0 text-sm text-gray-600 dark:text-gray-300'>
                       {user.user_metadata?.full_name || user.email}
                     </span>
                   </div>
